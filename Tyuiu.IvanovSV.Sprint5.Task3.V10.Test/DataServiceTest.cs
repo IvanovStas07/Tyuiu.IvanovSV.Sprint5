94abe5917cce0ad192047798a1e9eb24696051fe
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-29 04:20:32 +0500

    Task3

---
+++ Tyuiu.IvanovSV.Sprint5.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.IvanovSV.Sprint5.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.BerezovskayaGO.Sprint5\Tyuiu.BerezovskayaGO.Sprint5.Task3.V10\bin\Debug\net8.0\OutPutFileTask3.bin";
+
+            FileInfo fileUnfo = new FileInfo(path);
+            bool fileInfo = fileUnfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileInfo);
+        }
+    }
+}
