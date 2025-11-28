a616f8aeab1e1268fc1bdec9fda30df6829963e9
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-29 02:50:50 +0500

    Add project files.

---
+++ Tyuiu.IvanovSV.Sprint5.V5.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.IvanovSV.Sprint5.V5.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.IvanovSV.Sprint5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.IvanovSV.Sprint5.V5.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
