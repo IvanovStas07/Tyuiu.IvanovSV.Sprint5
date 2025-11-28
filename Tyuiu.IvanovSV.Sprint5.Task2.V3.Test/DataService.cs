08b95fef248e570d6524b62bec28e9f6b319181c
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-29 04:13:52 +0500

    Task2

---
+++ Tyuiu.IvanovSV.Sprint5.Task2.V3.Test/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.IvanovSV.Sprint5.Task2.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\vvvik\AppData\Local\Temp\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
