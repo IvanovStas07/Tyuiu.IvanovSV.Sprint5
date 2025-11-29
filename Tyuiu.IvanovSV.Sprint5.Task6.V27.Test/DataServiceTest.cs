91ee4b7fb5098948a21ec872a2dcfde3c93ccc3b
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-29 05:33:02 +0500

    Task6

---
+++ Tyuiu.IvanovSV.Sprint5.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+namespace Tyuiu.IvanovSV.Sprint5.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Станислав\Desktop\source\repos\Tyuiu.BubenkoLG.Sprint5\Tyuiu.BubenkoLG.Sprint5.Task6.V27\bin\Debug\net8.0\InPutDataFileTask6V27.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.IvanovSV.Sprint5.Task6.V27/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.IO;
+using Tyuiu.IvanovSV.Sprint5.Task6.V27.Lib;
+
+internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Иванов С. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                ");
+            Console.WriteLine("* Тема: обработка текстовых файлов.                                        ");
+            Console.WriteLine("* Задание #6                                                               ");
+            Console.WriteLine("* Вариант #27                                                              ");
+            Console.WriteLine("* Выполнил: Иванов С. В. | ИСТНб-25-1                                       ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+            Console.WriteLine("* Дан файл InPutDataFileTask6V27.txt, в котором есть набор символьных      ");
+            Console.WriteLine("* данных. Найти количество трехзначных чисел в заданной строке.            ");
+
+            string path = $@"{Path.GetTempPath()}\InPutDataFileTask5V5.txt";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
