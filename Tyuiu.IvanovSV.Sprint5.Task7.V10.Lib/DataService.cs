f5a173b3bd1e37c10042559016c9d96510c327c3
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-29 06:10:50 +0500

    Task7

---
+++ Tyuiu.IvanovSV.Sprint5.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.IvanovSV.Sprint5.Task7.V10.Lib
+{
+    public class DataService : ISprint5Task7V10
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V10.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+                File.Delete(pathSaveFile);
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line[i] >= 'A' && line[i] <= 'Z'))
+                            strLine = strLine + char.ToLower(line[i]);
+                        else
+                            strLine = strLine + line[i];
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
